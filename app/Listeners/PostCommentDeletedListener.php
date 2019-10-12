88519c9b5e84945ecc1319634f98892e26409ab7
cooooomeon <2236559110@qq.com>
2019-10-12 20:05:17 +0800

    '修改评论删除监听事件'

--- app/Listeners/PostCommentDeletedListener.php
+++ app/Listeners/PostCommentDeletedListener.php
@@ -32,11 +32,20 @@ class PostCommentDeletedListener
         //获取事件中保存的信息
         $object = $event->getObject();
         $post = $object->post;
-        $post->decrement('post_comment_num');
+        if(empty($post))
+        {
+            abort(404 , 'Post has been deleted');
+        }
         if($object->comment_comment_p_id===0)
         {
+            $post->decrement('post_comment_num');
             notify_remove([5] , $post);
         }else{
+            if(empty($object->parent))
+            {
+                abort(404 , 'Parent comment has been deleted');
+            }
+            $post->decrement('post_comment_num');
             notify_remove([6] , $object->parent);
         }
     }
