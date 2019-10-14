4084f6654d4e1a13ea1f693124716f03f17b7e84
cooooomeon <2236559110@qq.com>
2019-10-14 20:09:17 +0800

    '修改hook'

--- public/webhook.php
+++ public/webhook.php
@@ -3,7 +3,7 @@
 
 error_reporting(E_ALL);
 //需要自动部署的项目目录
-$dir =  '/home/wwwroot/test.api.yooul.net';
+$dir =  '/home/wwwroot/api.yooul.net';
 
 //'2>&1'是让执行管道输出结果。
-echo shell_exec("cd $dir && git pull origin master");
+echo shell_exec("cd $dir && git pull origin dev");
