33ce6ac911186763f24818648b8d789e1c7fdaa4
cooooomeon <2236559110@qq.com>
2019-10-15 13:45:57 +0800

    '添加.user.ini和webhook.php到忽略文件''

--- public/webhook.php
+++ public/webhook.php
@@ -3,7 +3,12 @@
 
 error_reporting(E_ALL);
 //需要自动部署的项目目录
-$dir =  '/home/wwwroot/api.yooul.net';
+$dir =  "..";
+$cmd = <<<DOC
+    cd $dir && git pull origin master && composer update  2>&1
+DOC;
 
 //'2>&1'是让执行管道输出结果。
-echo shell_exec("cd $dir && git pull origin dev");
+$log = shell_exec($cmd);
+file_put_contents('git.log' , 'date:'.date('Y-m-d H:i:s').'  log:'.$log.PHP_EOL , FILE_APPEND);
+echo json_encode(array('log'=>$log));die;
