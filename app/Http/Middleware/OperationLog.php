bea74c848f9aa8b717d62aed578363324a65db9b
cooooomeon <2236559110@qq.com>
2019-11-08 14:12:35 +0800

    操作日志添加事务

--- app/Http/Middleware/OperationLog.php
+++ app/Http/Middleware/OperationLog.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Auth;
 use Closure;
 use Carbon\Carbon;
-use App\Models\OperationLog as Operation;
+use Illuminate\Support\Facades\DB;
 use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
 
 class OperationLog extends BaseMiddleware
@@ -21,19 +21,27 @@ class OperationLog extends BaseMiddleware
     {
         if(auth()->check())
         {
-            $chinaNow = strtotime(Carbon::now('Asia/Shanghai'));
+            $chinaNow = Carbon::now('Asia/Shanghai');
             $user_id = (int) auth()->id();
-            $operation = Operation::where('user_id' , $user_id)
-                                    ->whereDate('created_at' , '>=' , date('Y-m-d 00:00:00' , $chinaNow))
-                                    ->whereDate('created_at' , '<=' , date('Y-m-d 23:59:59' , $chinaNow))
-                                    ->exists();
-            if(!$operation)
-            {
-                $log = new Operation();
-                $log->setAttribute('user_id', $user_id);
-                $log->setAttribute('ip', getRequestIpAddress());
-                $log->setAttribute('referer', $request->server('HTTP_REFERER'));
-                $log->save();
+            DB::beginTransaction();
+            try {
+                $count = DB::table('views_logs')->where('user_id' , $user_id)
+                    ->whereDate('created_at' , '>=' , date('Y-m-d 00:00:00' , strtotime($chinaNow)))
+                    ->whereDate('created_at' , '<=' , date('Y-m-d 23:59:59' , strtotime($chinaNow)))
+                    ->lockForUpdate()->count();
+                if ($count <= 0) {
+                    DB::table('views_logs')->insert(array(
+                        'user_id'=>$user_id,
+                        'ip'=>getRequestIpAddress(),
+                        'referer'=>$request->server('HTTP_REFERER'),
+                        'created_at'=>$chinaNow,
+                    ));
+                }
+                // 提交事务
+                DB::commit();
+            } catch (\Exception $e) {
+                // 回滚事务
+                DB::rollBack();
             }
         }
         return $next($request);
