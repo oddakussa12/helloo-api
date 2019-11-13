755c1261865c70970467b47857593d231ec979e8
Gxc <gxcphp@163.com>
2019-11-13 16:46:41 +0800

    增加chat_id

--- app/Resources/PyChatCollection.php
+++ app/Resources/PyChatCollection.php
@@ -4,7 +4,7 @@
  * @Author: Dell
  * @Date:   2019-11-13 11:42:21
  * @Last Modified by:   Dell
- * @Last Modified time: 2019-11-13 16:30:11
+ * @Last Modified time: 2019-11-13 16:42:18
  */
 namespace App\Resources;
 
@@ -20,6 +20,7 @@ class PyChatCollection extends Resource
             'user_avatar' => $this->user->user_avatar,
             'user_country' => $this->user->user_country,
             'user_is_guest' => $this->user->user_is_guest,
+            'chat_id' => $this->chat_id,
             'chat_default_locale' => $this->chat_default_locale,
             'chat_default_message' => $this->chat_default_message,
             'chat_locale' => $this->chat_locale,
