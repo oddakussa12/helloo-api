5113d7e9df743c82dad5c5a406f947dc509a1e5f
cooooomeon <2236559110@qq.com>
2019-11-13 19:25:11 +0800

    添加右键发送人

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'yooul@mail.yooul.world'),
+        'name' => env('MAIL_FROM_NAME', '小馒科技'),
     ],
 
     /*
