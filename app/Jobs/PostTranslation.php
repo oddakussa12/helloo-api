398bac62c789f2e76ed58647d51630d7571357b6
root <root@ip-172-31-10-108.cn-north-1.compute.internal>
2020-03-30 17:31:00 +0800

    优化翻译接口

--- app/Jobs/PostTranslation.php
+++ app/Jobs/PostTranslation.php
@@ -88,7 +88,7 @@ class PostTranslation implements ShouldQueue
         {
             if($l=='zh-HK')
             {
-                $t = 'zh-TW';
+                continue;
             }else{
                 $t = $l;
             }
@@ -130,6 +130,9 @@ class PostTranslation implements ShouldQueue
             ]);
             $post->save();
         }
+        $post->translate('zh-HK')->post_title = $post->translate('zh-TW')->post_title;
+        $post->translate('zh-HK')->post_content = $post->translate('zh-TW')->post_content;
+        $post->save();
         $exceptLanguages = array_unique(array_diff($exceptLanguages , $languages));
         foreach ($exceptLanguages as $l)
         {
