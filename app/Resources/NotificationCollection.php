633004c291ff1ca6f6e94f7fa9925d0bc1b4f9f3
root <root@ip-172-31-10-108.cn-north-1.compute.internal>
2020-03-31 11:52:15 +0800

    消息时间格式化

--- app/Resources/NotificationCollection.php
+++ app/Resources/NotificationCollection.php
@@ -3,8 +3,7 @@
 
 namespace App\Resources;
 
-use App\Repositories\Contracts\PostCommentRepository;
-use App\Repositories\Contracts\PostRepository;
+use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\Resource;
 
 class NotificationCollection extends Resource
@@ -28,7 +27,32 @@ class NotificationCollection extends Resource
                 return new UserCollection($this->from);
             }),
             'created_at'=>optional($this->created_at)->toDateTimeString(),
+            'format_created_at'=>$this->getPostFormatCreatedAt(),
         ];
+    }
+
+    private function getPostFormatCreatedAt()
+    {
+        $locale = locale();
+        if($locale=='zh-CN')
+        {
+            Carbon::setLocale('zh');
+        }elseif ($locale=='zh-TW'||$locale=='zh-HK')
+        {
+            Carbon::setLocale('zh_TW');
+        }else{
+            $locale = 'en';
+            Carbon::setLocale($locale);
+            $translator = \Carbon\Translator::get($locale);
+            $translator->setMessages($locale , [
+                'minute' => ':count m|:count m',
+                'hour' => ':count h|:count h',
+                'day' => ':count d|:count d',
+                'month' => ':count mo|:count mo',
+                'year' => ':count yr|:count yr',
+            ]);
+        }
+        return Carbon::parse($this->created_at)->diffForHumans();
     }
 //
 //    private function details()
