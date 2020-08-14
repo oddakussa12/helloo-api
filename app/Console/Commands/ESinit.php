58e180c13a2a85b1b6ee0aa39902396c22a59575
tsmtang <t349359058@sina.com>
2020-08-14 11:06:18 +0800

    es init

--- app/Console/Commands/ESinit.php
+++ app/Console/Commands/ESinit.php
@@ -4,23 +4,15 @@ namespace App\Console\Commands;
 
 use GuzzleHttp\Client;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 
 class ESinit extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'command:name';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
-
+    //使用什么命令启动脚本
+    protected $signature = 'es:init';
+    //描述
+    protected $description = 'init laravel es for post';
     /**
      * Create a new command instance.
      *
@@ -38,79 +30,70 @@ class ESinit extends Command
      */
     public function handle()
     {
-        //
+        try{
+//创建template
+            $client = new \GuzzleHttp\Client(); //这里的Clinet()是你vendor下的GuzzleHttp下的Client文件
+            $this->createTemplate($client);
+            $this->info('============create template success============');
+            $this->createIndex($client);
+            $this->info('============create index success============');
+        }catch (\Exception $e){
+            ownLogs('test.log', $e->getMessage());
+        }
 
-        //创建template
-        $client = new Client(); //这里的Clinet()是你vendor下的GuzzleHttp下的Client文件
-        $url = config('scout.elasticsearch.hosts')[0].'/inssa';   //这里写logstash配置中index参数
-        $client->delete($url);//确定没有这个url
+    }
+    /**
+     * 创建模板 see https://www.elastic.co/guide/en/elasticsearch/reference/current/dynamic-templates.html
+     * @param Client $client
+     */
+    private function createTemplate($client)
+    {
+        $url = config('scout.elasticsearch.hosts')[0] . '/_template/template_1';
+// $client->delete($url);
+        $client->put($url, [
 
-        /*
-         * 这个模板作用于我要做用的索引
-         * */
-        $param = [
-            'json'=>[
-                /*
-                 * 这句是取在scout.php（scout是驱动）里我们配置好elasticsearch引擎的
-                 * index项。
-                 * PS：其实都是取数组项，scout本身就是return一个数组，
-                 * scout.elasticsearch.index就是取
-                 * scout[elasticsearch][index]
-                 * */
-                'template'=>config('scout.elasticsearch.index'),
-                'mappings'=>[
-                    '_default_'=>[
-                        'dynamic_templates'=>[
-                            [
-                                'string'=>[
-                                    'match_mapping_type'=>'string',//传进来的是string
-                                    'mapping'=>[
-                                        'type'=>'text',//把传进来的string按text（文本）处理
-                                        'analyzer'=>'ik_smart',//用ik_smart进行解析（ik是专门解析中的插件）
-                                        'fields'=>[
-                                            'title'=>[
-                                                'type'=>'text'
-                                            ],
-                                            'keyword'=>[
-                                                'type'=>'keyword'
-                                            ],
-                                        ]
-                                    ]
+            'json' => [
+                'index_patterns' => [config('scout.elasticsearch.index').'*'],
+                'settings' => [
+                    'number_of_shards' => 1,
+                ],
+                'mappings' => [
+                    '_source' => [
+                        'enabled' => true
+                    ],
+                    'properties' => [
+                        'mapping' => [ // 字段的处理方式
+                            'type' => 'keyword', // 字段类型限定为 string
+                            'fields' => [
+                                'raw' => [
+                                    'type' => 'keyword',
+                                    'ignore_above' => 256, // 字段是索引时忽略长度超过定义值的字段。
                                 ]
-                            ]
-                        ]
-                    ]
+                            ],
+                        ],
+                    ],
+
                 ],
             ],
-        ];
-        $client->put($url,$param);
+        ]);
+    }
 
-        $this->info('============create template success============');
-
-//创建index
-        $url = config('scout.elasticsearch.hosts')[0].'/'.config('scout.elasticsearch.index');
-//$client->delete($url);
-
-        $param = [
-            'json'=>[
-                'settings'=>[
-                    'refresh_interval'=>'5s',
-                    'number_of_shards'=>1,
-                    'number_of_replicas'=>0,
+    /**
+     * 创建索引
+     * @param Client $client
+     */
+    private function createIndex($client)
+    {
+        $url = config('scout.elasticsearch.hosts')[0] . '/' . config('scout.elasticsearch.index');
+        // $client->delete($url);
+        $client->put($url, [
+            'json' => [
+                'settings' => [
+                    'refresh_interval' => '5s',
+                    'number_of_shards' => 1, // 分片为
+                    'number_of_replicas' => 0, // 副本数
                 ],
-
-                'mappings'=>[
-                    '_default_'=>[
-                        '_all'=>[
-                            'enabled'=>false
-                        ]
-                    ]
-                ]
-            ]
-        ];
-
-        $client->put($url,$param);
-        $this->info('============create index success============');
-
+            ],
+        ]);
     }
 }
