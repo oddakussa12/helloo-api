a625324554a211dc3789c5c18bbc8b519f7e0b0c
tsmtang <t349359058@sina.com>
2020-08-19 19:15:04 +0800

    device info

--- app/Http/Controllers/V1/DeviceController.php
+++ app/Http/Controllers/V1/DeviceController.php
@@ -19,32 +19,31 @@ class DeviceController extends BaseController
      * @var string
      */
     private $mIndex;
+    private $postIndex;
 
     /**
      * @var DeviceRepository
      */
-
     public function __construct()
     {
+        $this->postIndex = env('ELASTICSEARCH_POST_INDEX');
     }
 
     public function test()
     {
-
         set_time_limit(0);
 
         $countSql = "SELECT count(1) num
-FROM f_posts_translations t
-inner join f_posts p on p.post_id = t.post_id
-where p.post_created_at > '2020-01-01'
-ORDER BY t.post_id desc;";
-
+                    FROM f_posts_translations t
+                    inner join f_posts p on p.post_id = t.post_id
+                    where p.post_created_at > '2020-01-01'
+                    ORDER BY t.post_id desc;";
 
         $countResult = DB::select($countSql);
 
         $count  = $countResult[0]->num;
         $limit  = 1000;
-        $page   = intval(ceil($count/$limit));
+        $page   = ceil($count/$limit);
         sleep(1);
         dump($count, $limit, $page);
 
@@ -52,21 +51,6 @@ ORDER BY t.post_id desc;";
             $offset = $limit*$i;
             $sql = "
         SELECT p.post_id,p.post_uuid,p.user_id,p.post_category_id,p.post_media,p.post_content_default_locale,p.post_type,
-/*MAX(CASE t.`post_locale` WHEN 'en' THEN t.`post_content` ELSE '' END) as 'en',
-MAX(CASE t.`post_locale` WHEN 'id' THEN t.`post_content` ELSE '' END) as 'hindi',
-MAX(CASE t.`post_locale` WHEN 'zh-CN' THEN t.`post_content` ELSE '' END) as 'zhCN',
-MAX(CASE t.`post_locale` WHEN 'ar' THEN t.`post_content` ELSE '' END) as 'ar',
-MAX(CASE t.`post_locale` WHEN 'hi' THEN t.`post_content` ELSE '' END) as 'hi',
-MAX(CASE t.`post_locale` WHEN 'ko' THEN t.`post_content` ELSE '' END) as 'ko',
-MAX(CASE t.`post_locale` WHEN 'ja' THEN t.`post_content` ELSE '' END) as 'ja',
-MAX(CASE t.`post_locale` WHEN 'es' THEN t.`post_content` ELSE '' END) as 'es',
-MAX(CASE t.`post_locale` WHEN 'zh-TW' THEN t.`post_content` ELSE '' END) as 'zhTW',
-MAX(CASE t.`post_locale` WHEN 'zh-HK' THEN t.`post_content` ELSE '' END) as 'zhHK',
-MAX(CASE t.`post_locale` WHEN 'vi' THEN t.`post_content` ELSE '' END) as 'vi',
-MAX(CASE t.`post_locale` WHEN 'th' THEN t.`post_content` ELSE '' END) as 'th',
-MAX(CASE t.`post_locale` WHEN 'fr' THEN t.`post_content` ELSE '' END) as 'fr',
-MAX(CASE t.`post_locale` WHEN 'de' THEN t.`post_content` ELSE '' END) as 'de',
-MAX(CASE t.`post_locale` WHEN 'ru' THEN t.`post_content` ELSE '' END) as 'ru',*/
 t.post_locale, t.post_content,
 p.post_created_at as create_at
 FROM f_posts_translations t
@@ -79,20 +63,20 @@ ORDER BY t.post_id desc
 
             $result = DB::select($sql);
             $result = array_map('get_object_vars', $result);
-            echo '-----------'. env('ELASTICSEARCH_POST_INDEX');
+            echo '-----------'. $this->postIndex;
 
-            $data = (new Es('post222'))->batchCreate($result);
+            $data = (new Es($this->postIndex))->batchCreate($result);
             if ($data==null) {
-                $data = (new Es('post222'))->batchCreate($result);
+                (new Es($this->postIndex))->batchCreate($result);
             }
-            //dump($data);
-            flush();
-            ob_flush();
+            dump('foreach:: '. $offset);
         }
 
+        return;
 
-        dump(123);
-        exit;
+
+
+       /* exit;
         (new Es('post'))->generateDoc($result);
 
         exit;
@@ -111,7 +95,7 @@ ORDER BY t.post_id desc
         $device->device_registration_id =time();
         $device->user_id = rand(3300, 3500);
         dump($device);
-        $device->save();
+        $device->save();*/
 
     }
     public function index()
