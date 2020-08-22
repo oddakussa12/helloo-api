440be30ef35ab202259c182867e409aa97fff64d
tsmtang <t349359058@sina.com>
2020-08-22 11:35:36 +0800

    post  insert  ES

--- app/Jobs/PostTranslationV2.php
+++ app/Jobs/PostTranslationV2.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Models\Es;
 use App\Models\User;
 use App\Models\Post;
 use App\Traits\CachableUser;
@@ -143,6 +144,26 @@ class PostTranslationV2 implements ShouldQueue
                 ['post_id' => $post->getKey(), 'post_locale' => $l] ,
                 ['post_title'=>$title , 'post_content'=>$content]
             );
+        }
+
+
+        // 组装数据 插入ES
+
+        $post->post_uuid  = $post->post_uuid->toString();
+        $post->create_at  = optional($post->post_created_at)->toDateTimeString();
+        $post->post_media = (!empty($post->post_type) && !empty($post->post_media)) ? postMedia($post->post_type, $post->post_media) : null;
+
+        $result           = $post->getAttributes();
+        $postInfo         = $post->getTranslationsArray();
+
+        unset($result['post_country_id'], $result['post_rate'], $result['post_event_country_id'], $result['post_created_at']);
+
+        $postList = array_map(function($v) use ($result){unset($v['post_title']);return array_merge($result, $v);}, $postInfo);
+        $postList = array_column($postList, null);
+
+        $data     = (new Es(config('scout.elasticsearch.post')))->batchCreate($postList);
+        if ($data==null) {
+            $data = (new Es(config('scout.elasticsearch.post')))->batchCreate($postList);
         }
     }
 }
