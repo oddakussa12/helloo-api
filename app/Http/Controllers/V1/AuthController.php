48b7ad951f56ed3501a0a154cdd197ac0fa76329
Hao Zheng <2236559110@qq.com>
2020-08-28 00:30:19 +0800

    恢复手机号注册方法

--- app/Http/Controllers/V1/AuthController.php
+++ app/Http/Controllers/V1/AuthController.php
@@ -334,14 +334,9 @@ class AuthController extends BaseController
         }else{
             $user_fields['user_country_id'] = getUserCountryId($addresses->iso_code);
         }
-        /// \DB::beginTransaction();
+        \DB::beginTransaction();
         try{
-            $user = (new User($user_fields))->save();
-            $userId = $user->id;
-            //exit;
-            //$userId = (new User())->se;
-            //$userId = \DB::table('users')->insertGetId($user_fields);
-            
+            $userId = \DB::table('users')->insertGetId($user_fields);
             \DB::table('users_phones')->insert(array('user_id'=>$userId , 'user_phone'=>$user_phone , 'user_phone_country'=>$user_phone_country));
             \DB::commit();
         }catch (\Exception $e)
