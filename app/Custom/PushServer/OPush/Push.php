82c42a6bc1983ae202f7a107f5ec5167aebc33c6
tsmtang <t349359058@sina.com>
2020-09-03 15:44:02 +0800

    oppo push test

--- app/Custom/PushServer/OPush/Push.php
+++ app/Custom/PushServer/OPush/Push.php
@@ -26,7 +26,8 @@ class Push
         $this->token   = array_get($params, 'registrationId');
         $this->extra   = array_get($params, 'extras');
         $this->type    = 1;
-        $this->intent  = 'intent://com.yooul/deeplink?#Intent;scheme=pushscheme;launchFlags=0x4000000;S.type=';
+//        $this->intent  = 'intent://com.yooul/deeplink?#Intent;scheme=pushscheme;launchFlags=0x4000000;S.type=';
+        $this->intent  = 'com.yooul.oppopush';
 
         //实例oppo
         $this->client = new oppoPush(config('push.oppo.appKey'), config('push.oppo.masterSecret')); // AppKey 与 MasterSecret(非 AppSecret)
@@ -44,9 +45,9 @@ class Push
             ->setContent($this->desc)
             ->setAuthToken($this->authToken);
         $this->client->getAuthTokenExpiresTime();           // 获取 auth_token 过期时间
-        $this->client->setIntent($this->intent.$this->extra['type'].";end");           // 打开应用内页的 intent action
+        $this->client->setIntent($this->intent);// 打开应用内页的 intent action
         //$this->client->setActionUrl('http://www.xxx.com');// 打开网页
-        $this->client->setActionParameters($this->payload); // 打开应用内页或网页时传递的参数 (数组或json类型)
+        $this->client->setActionParameters(['type'=>$this->extra['type']]); // 打开应用内页或网页时传递的参数 (数组或json类型)
         $this->client->addRegistrationId($this->token);     // 添加需要发送设备的 registration_id, 最多 1000 个
         try {
            return $this->client->broadcastByRegId();              // registration_id 推送
