8298673ec5d9c8e544c01cb9c828794dcd03f20c
tsmtang <t349359058@sina.com>
2020-09-08 11:23:54 +0800

    热门话题 redis

--- app/Http/Controllers/V1/TopicController.php
+++ app/Http/Controllers/V1/TopicController.php
@@ -125,11 +125,13 @@ class TopicController extends BaseController
         $key    = 'hot_topic';
         $result = Redis::get($key);
         $time   = Carbon::now();
+
         if(empty($result)) {
-            $topics = DB::select('SELECT topic_content,flag,sort from f_hot_topics where is_delete<1 and (start_time > ? and end_time < ?) GROUP by topic_content ORDER BY flag asc sort desc limit 20', [$time, $time]);
+            $topics = DB::select('SELECT topic_content,flag,sort from f_hot_topics where is_delete<1 and (start_time >= ? and end_time <= ?) GROUP by topic_content ORDER BY flag asc, sort desc limit 20', [$time, $time]);
             $result = sortArrByManyField($topics,'flag',SORT_ASC,'sort',SORT_DESC);
-            $result = json_encode($result, JSON_UNESCAPED_UNICODE);
-            Redis::set($key, $result);
+            if(!empty($topics)) {
+                Redis::set($key, json_encode($topics, JSON_UNESCAPED_UNICODE));
+            }
         }
 
         $result = $result ? json_decode($result, true) : [];
