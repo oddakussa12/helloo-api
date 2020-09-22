94b471d7aad68147cb141b13ee1386aa29f85275
2236559110@qq.com <2236559110@qq.com>
2020-09-22 10:25:40 +0800

    fix per day push

--- resources/lang/de/push.php
+++ resources/lang/de/push.php
@@ -3,12 +3,12 @@ return [
     "app_name"=>"Yooul",
     "i_like_you_i_want_to_be_friends_with_you_okay"=>"Ich mag dich ~ Ich möchte mit dir befreundet sein, okay?",
     "come_on"=>"Komm schon! ",
-    "there_are_persons_viewed_your_homepage"=>"Es gibt ### Personen, die Ihre Homepage angesehen haben",
-    "there_are_persons_who_have_viewed_your_posts"=>"Es gibt ### Personen, die Ihre Beiträge angesehen haben",
-    "you_people_are_interested_in_you"=>"Sie ### Leute interessieren sich für Sie",
+    "there_are_persons_viewed_your_homepage"=>"Es gibt :attribute Personen, die Ihre Homepage angesehen haben",
+    "there_are_persons_who_have_viewed_your_posts"=>"Es gibt :attribute Personen, die Ihre Beiträge angesehen haben",
+    "you_people_are_interested_in_you"=>"Sie :attribute Leute interessieren sich für Sie",
     "your_friend_visited_your_personal_center"=>"Ihr Freund hat Ihr persönliches Zentrum besucht",
     "someone_chatted_with_you_privately_go_and_have_a_look"=>"Jemand hat sich privat mit Ihnen unterhalten, gehen Sie und schauen Sie",
-    "hiiiiiiiiiiiii"=>"Hiiiiiiiiiiiii!",
+    "hi"=>"Hiiiiiiiiiiiii!",
     "can_i_be_your_friend"=>"Kann ich dein Freund sein?",
     "i_am_from_america_where_are_you_from"=>"Ich komme aus Amerika, woher kommst du?"
 ];
