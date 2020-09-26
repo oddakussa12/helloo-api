4e0e322ca8d67d7d7a18ee31c282002c36525d7e
2236559110@qq.com <2236559110@qq.com>
2020-09-26 18:16:35 +0800

    update RyChat field

--- app/Jobs/RyChat.php
+++ app/Jobs/RyChat.php
@@ -93,41 +93,41 @@ class RyChat implements ShouldQueue
                 'chat_from_id'=>$raw['fromUserId'],
                 'chat_to_id'=>$raw['toUserId'],
                 'chat_msg_type'=>$raw['objectName'],
-                'chat_channel_type'=>$raw['channelType'],
+//                'chat_channel_type'=>$raw['channelType'],
                 'chat_time'=>$raw['msgTimestamp'],
-                'chat_sensitive_type'=>$raw['sensitiveType']
+//                'chat_sensitive_type'=>$raw['sensitiveType']
             );
             if(isset($raw['source']))
             {
                 $data['chat_source'] = $raw['source'];
             }
-            if(isset($raw['groupUserIds']))
-            {
-                $data['chat_group_to_id'] = $raw['groupUserIds'];
-            }
+//            if(isset($raw['groupUserIds']))
+//            {
+//                $data['chat_group_to_id'] = $raw['groupUserIds'];
+//            }
             if(isset($raw['content']))
             {
                 $content = \json_decode($raw['content'] , true);
-                if(isset($content['content']))
-                {
-                    $data['chat_content'] = $content['content'];
-                }
-                if(isset($content['imageUri']))
-                {
-                    $data['chat_image'] = $content['imageUri'];
-                }
+//                if(isset($content['content']))
+//                {
+//                    $data['chat_content'] = $content['content'];
+//                }
+//                if(isset($content['imageUri']))
+//                {
+//                    $data['chat_image'] = $content['imageUri'];
+//                }
                 if(isset($content['user']))
                 {
                     $data['chat_from_name'] = $content['user']['name'];
                 }
-                if(isset($content['user']['extra']))
-                {
-                    $data['chat_from_extra'] = \json_encode($content['user']['extra'] , JSON_UNESCAPED_UNICODE);
-                }
+//                if(isset($content['user']['extra']))
+//                {
+//                    $data['chat_from_extra'] = \json_encode($content['user']['extra'] , JSON_UNESCAPED_UNICODE);
+//                }
             }
             $ryChat = RyChats::create($data);
 
-            RyChatRaw::create(array('chat_id'=>$ryChat->chat_id , 'raw'=>\json_encode($raw , JSON_UNESCAPED_UNICODE),'chat_time'=>$raw['msgTimestamp']));
+//            RyChatRaw::create(array('chat_id'=>$ryChat->chat_id , 'raw'=>\json_encode($raw , JSON_UNESCAPED_UNICODE),'chat_time'=>$raw['msgTimestamp']));
 
         }
 
