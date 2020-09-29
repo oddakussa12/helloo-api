f0004c323692b8ee92087ebdddbf0b9412dd8d88
tsmtang <t349359058@sina.com>
2020-09-29 15:53:29 +0800

    push  add log

--- app/Custom/PushServer/PushServer.php
+++ app/Custom/PushServer/PushServer.php
@@ -66,7 +66,7 @@ class PushServer
                     return true;
                 } else {
                     if (!empty($result['tokensToDelete'])) {
-                         //Log::info(__FUNCTION__.' Push message tokensToDelete:'.__FUNCTION__, $result);
+                         Log::info(__FUNCTION__.' Push message tokensToDelete:'.__FUNCTION__, $result);
                         return true;
                     } else {
                         Log::info(__FUNCTION__.' Push message fail:'.__FUNCTION__, $result);
@@ -105,7 +105,7 @@ class PushServer
         try {
             $result = (new MiPush($this->params))->send();
             if ($result['code'] == 0) {
-                // Log::info(__FUNCTION__.' Push message success:'.__FUNCTION__, $result);
+                Log::info(__FUNCTION__.' Push message success:'.__FUNCTION__, $result);
                 return true;
             } else {
                 Log::info(__FUNCTION__.' Push message fail:'.__FUNCTION__, $result);
@@ -121,7 +121,7 @@ class PushServer
         try {
             $result = (new OPush($this->params))->send();
             if ($result['code'] == 0) {
-                // Log::info(__FUNCTION__.' Push message success:'.__FUNCTION__, $result);
+                Log::info(__FUNCTION__.' Push message success:'.__FUNCTION__, $result);
                 return true;
             } else {
                 Log::info(__FUNCTION__.' Push message fail:'.__FUNCTION__, $result);
