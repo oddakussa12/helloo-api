dac9cb4e61a214ae2c738d1a246ee831e52ae360
2236559110@qq.com <2236559110@qq.com>
2020-09-30 10:52:39 +0800

    feature rate test

--- app/Console/Commands/CalculatingRateLimitV2.php
+++ app/Console/Commands/CalculatingRateLimitV2.php
@@ -8,6 +8,7 @@ use App\Traits\CachablePost;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Storage;
+use function GuzzleHttp\Psr7\str;
 
 class CalculatingRateLimitV2 extends Command
 {
@@ -79,7 +80,7 @@ class CalculatingRateLimitV2 extends Command
                 $commenterCount= $this->commenterCount($postId);
                 $countryCount = $this->countryNum($postId);
                 $rate = rate_comment_v4($commentCount , Carbon::createFromTimestamp($time)->toDateTimeString() , $likeCount , $commenterCount , $countryCount , $postGravity);
-                Storage::prepend('rate_'.strval($index).'.log', strval($postId).'--rate:'.$rate.'--postGravity:'.$postGravity.PHP_EOL);
+                Storage::prepend('rate_'.strval($index).'.log', strval($postId).','.strval($rate).','.strval($likeCount).','.str($postGravity).PHP_EOL);
 //                Redis::zadd($rateKey , rate_comment_v3($commentCount , Carbon::createFromTimestamp($time)->toDateTimeString() , $likeCount , $commenterCount , $countryCount) , $postId);
                 Redis::zadd($rateV2Key , $rate , $postId);
             }
