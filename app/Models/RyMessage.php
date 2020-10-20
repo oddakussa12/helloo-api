92e1f19ac66fa8fa95ce9170f2c38c584d0eee01
2236559110@qq.com <2236559110@qq.com>
2020-10-20 17:45:42 +0800

    add ry message store

---
+++ app/Models/RyMessage.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class RyMessage extends Model
+{
+
+
+    protected $table = "ry_messages";
+
+    const CREATED_AT = 'created_at';
+
+    protected $primaryKey = 'id';
+
+
+    protected $fillable = [
+        'message_id',
+        'message_content',
+        'message_time',
+    ];
+
+    public function setUpdatedAt($value)
+    {
+
+    }
+
+
+}
