ff3a948d1cfe4dfc53b1a1eca4edffa75a6101a6
2236559110@qq.com <2236559110@qq.com>
2020-10-23 11:23:56 +0800

    fix service message

--- app/Console/Commands/AllUserNotification.php
+++ app/Console/Commands/AllUserNotification.php
@@ -88,6 +88,14 @@ class AllUserNotification extends Command
                 if(blank($msg['image']))
                 {
                     unset($msg['image']);
+                }else{
+                    $domain = config('common.qnUploadDomain.thumbnail_domain');
+                    $images = $msg['image'];
+                    foreach ($images as $lang=>$image)
+                    {
+                        $images[$lang] = $domain.$image."?imageMogr2/auto-orient/interlace/1|imageslim";
+                    }
+                    $msg['image'] = $images;
                 }
                 $content = array(
                     'userInfo'=>array(
@@ -96,8 +104,8 @@ class AllUserNotification extends Command
                 );
                 $content = array_merge($content , $msg);
 
-
-                /**test start**/
+//
+//                /**test start**/
 //                $userIds = array(31666 , 28752 , 28134 , 31619);
 //                $result = $person->send(array(
 //                    'senderId'   => $senderId,
@@ -105,7 +113,7 @@ class AllUserNotification extends Command
 //                    "objectName" => $objectName,
 //                    'content'    => \json_encode($content)
 //                ));
-                /**test end**/
+//                /**test end**/
 
 
                 for ($page=1;$page<=$lastPage;$page++)
