73f1b2a6e7294baa2f9a244cc68850bd3fdf3590
2236559110@qq.com <2236559110@qq.com>
2020-10-26 16:45:46 +0800

    fix ry user update time

--- app/Jobs/RyOnline.php
+++ app/Jobs/RyOnline.php
@@ -17,6 +17,8 @@ class RyOnline implements ShouldQueue
 
     private $chinaNow;
 
+    private $chinaDateTime;
+
     private $users;
     /**
      * @var int
@@ -32,6 +34,7 @@ class RyOnline implements ShouldQueue
     public function __construct($users)
     {
         $this->chinaNow = Carbon::now('Asia/Shanghai');
+        $this->chinaDateTime = $this->chinaNow->toDateTimeString();
         $this->time = $this->chinaNow->timestamp;
         $this->date = date('Y-m-d H:i:s' , $this->time);
         $this->users = $users;
@@ -116,6 +119,7 @@ class RyOnline implements ShouldQueue
                             'games' => $games,
                             'sports' => $sports,
                             'created_at'=>$this->time,
+                            'updated_at'=>$this->time,
                             'user_created_at'=>$user_created_at??$this->date,
                         )
                     );
@@ -141,7 +145,7 @@ class RyOnline implements ShouldQueue
                         'user_id'=>$userId,
                         'ip'=>'127.0.0.1',
                         'referer'=>'ry',
-                        'created_at'=>$chinaNow->toDateTimeString()
+                        'created_at'=>$this->chinaDateTime
                     ));
                 }
             }
