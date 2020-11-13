0d42a7de3fb9fe9926be34e685a519caee8c321f
Hao Zheng <2236559110@qq.com>
2020-11-13 23:11:01 +0800

    remove crond remove user

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,8 +25,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('remove:random_user')
-            ->everyMinute();
+//        $schedule->command('remove:random_user')
+//            ->everyMinute();
         $schedule->command('geoip:update')
             ->daily();
     }
