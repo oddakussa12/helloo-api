e52d8323b8f5da48779c299c2b5a4ca6aa293030
2236559110@qq.com <2236559110@qq.com>
2020-11-18 13:54:38 +0800

    test

--- app/Http/Controllers/V1/TestController.php
+++ app/Http/Controllers/V1/TestController.php
@@ -10,6 +10,8 @@ use Illuminate\Http\Request;
 use App\Messages\SignInMessage;
 use App\Custom\EasySms\PhoneNumber;
 use App\Custom\Uuid\RandomStringGenerator;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Log;
 use Overtrue\EasySms\Exceptions\NoGatewayAvailableException;
 
 
@@ -22,15 +24,32 @@ class TestController extends BaseController
         $number = new PhoneNumber(7529908826 , 44);
         try{
             $result = $sms->send($number, new SignInMessage(1234) , array('yunXinCustom'));
-            \Log::error($result);
+            Log::error($result);
         }catch (NoGatewayAvailableException $e)
         {
             $exception = $e->getLastException();
-            \Log::error($exception->getMessage());
+            Log::error($exception->getMessage());
         }
 
     }
-
+    public function redis()
+    {
+        $period = 1000;
+        $key = "test_{test}";
+        $this->now = $now = millisecond();   # 毫秒时间戳
+        $redis = Redis::connection('single');
+        $redis->multi(); //使用管道提升性能
+        $redis->zadd($key, $now, $now); //value 和 score 都使用毫秒时间戳
+        $redis->zremrangebyscore($key, 0, $now - $period * 1000); //移除时间窗口之前的行为记录，剩下的都是时间窗口内的
+        $redis->zcard($key);  //获取窗口内的行为数量
+        $redis->zrangebyscore($key , "-inf" , "+inf" , array(
+            'withScores'=>true,
+            'limit'=>array(0,1)
+        ));
+        $redis->expire($key, $period  + 1);  //多加一秒过期时间
+        $replies = $redis->exec();
+        return array('count'=>intval($replies[2]) , 'first'=>array_first($replies[3]));
+    }
     public function push()
     {
         $user = app(UserRepository::class)->find(94);
@@ -65,21 +84,22 @@ class TestController extends BaseController
 ,
             'extra'=>array('user'=>1234567890),
         ));
-        \Log::error($content);
-        \Log::error(\json_encode($result , JSON_UNESCAPED_UNICODE));
+        Log::error($content);
+        Log::error(\json_encode($result , JSON_UNESCAPED_UNICODE));
     }
 
 
     public function token()
     {
+        Log::error($this->redis());
         if(domain()!=config('app.url'))
         {
-            $token = app('rcloud')->getUser()->register(array(
-                'id'=> time(),
-                'name'=> (new RandomStringGenerator())->generate(16),
-                'portrait'=> "https://qnwebothersia.mmantou.cn/default_avatar.jpg?imageView2/0/w/50/h/50/interlace/1|imageslim"
-            ));
-            return $this->response->array($token);
+//            $token = app('rcloud')->getUser()->register(array(
+//                'id'=> time(),
+//                'name'=> (new RandomStringGenerator())->generate(16),
+//                'portrait'=> "https://qnwebothersia.mmantou.cn/default_avatar.jpg?imageView2/0/w/50/h/50/interlace/1|imageslim"
+//            ));
+//            return $this->response->array($token);
         }
     }
 
