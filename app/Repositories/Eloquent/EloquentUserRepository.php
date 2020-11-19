660d12eb0e66f5b17c1470c517d765ecb0e4fa54
Hao Zheng <2236559110@qq.com>
2020-11-20 00:05:43 +0800

    fix random viveo text

--- app/Repositories/Eloquent/EloquentUserRepository.php
+++ app/Repositories/Eloquent/EloquentUserRepository.php
@@ -389,9 +389,9 @@ class EloquentUserRepository  extends EloquentBaseRepository implements UserRepo
                         $text = "Selamat ! Anda telah dipertemukan dengan Artis Helloo";
                     }elseif($locale=='zh-CN')
                     {
-                        $text = 'Bingo！🎉 You have been matched with a Helloo Star.';
-                    }else{
                         $text = '恭喜您匹配到了Helloo Star.';
+                    }else{
+                        $text = 'Bingo！🎉 You have been matched with a Helloo Star.';
                     }
                     $data['official'] = $text;
                 }
