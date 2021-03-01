6ae03a66f4c75ebb3a7a54b4f4b5510c096eb524
2236559110@qq.com <2236559110@qq.com>
2021-03-01 14:28:17 +0800

    fix retention

--- app/Console/Commands/Retention.php
+++ app/Console/Commands/Retention.php
@@ -245,7 +245,7 @@ class Retention extends Command
             'new'=>$threeDateSignUpCount,
             '3'=>$threeDateKeepCount
         );
-        $result = DB::table('data_retentions')->where('country' , $country)->where('date' , $three)->update($sevenData);
+        $result = DB::table('data_retentions')->where('country' , $country)->where('date' , $three)->update($threeData);
         Log::info('three_result' , array(
             $threeData,$result,$three,$country
         ));
