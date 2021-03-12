0c2475d3860da4d0b1fc4334a25fb118df2edc60
2236559110@qq.com <2236559110@qq.com>
2021-03-12 21:55:22 +0800

    fix school search

--- app/Http/Controllers/V1/SchoolController.php
+++ app/Http/Controllers/V1/SchoolController.php
@@ -17,7 +17,7 @@ class SchoolController extends BaseController
         if(!blank($key)&&strlen($key)==mb_strlen($key))
         {
             $key = escape_like($key);
-            $key = addcslashes ($key);
+            $key = addcslashes($key);
             $userSchools = DB::table('users_schools_logs')->where('school' , 'like' , '%'.$key.'%')->orderByRaw("length(REPLACE(school,'{$key}',''))/length(school) desc")->select('school')->distinct()->limit(5)->pluck('school')->toArray();
         }
         return $this->response->array(array('data'=>$userSchools));
