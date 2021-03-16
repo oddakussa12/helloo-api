fdbaf32770a1f0d00750350d338b6bb97b53382c
2236559110@qq.com <2236559110@qq.com>
2021-03-16 14:45:09 +0800

    fix chat depth

--- app/Console/Commands/Test.php
+++ app/Console/Commands/Test.php
@@ -53,7 +53,36 @@ class Test extends Command
      */
     public function handle()
     {
-        $this->fillSchool();
+        $schools = array(
+            "Sekolah Menengah Atas Negri 10",
+            "Addis Ababa University",
+            "Colégio São Pedro",
+            "ESTV - GTI/STM Becora",
+            "Ensino Secundário 5 de Maio",
+            "Ensino Secundário 28 de Novembro",
+            "Nobel da Paz ",
+            "Ensino Secundário 12 de Novembro",
+            "Escola Técnica Informática ETI ",
+            "Escola An-Nur",
+            "Ensino Secundário Nicolao Lobato",
+            "São José Operário",
+            "Universidade Nacionál de Timor Lorosa'e",
+        );
+        $dates = array();
+        $startDate = "2021-01-01";
+        $endTime = "2021-03-14";
+        do{
+            array_push($dates , $startDate);
+            $startDate = Carbon::createFromFormat('Y-m-d' , $startDate)->addDays(1)->toDateString();
+        }while($startDate <= $endTime);
+        foreach ($schools as $school)
+        {
+            foreach ($dates as $date)
+            {
+                $command = "chat:depth school \"".$school."\" ".$date;
+                $this->call($command);
+            }
+        }
     }
 
     public function fillSchool()
