66d3fa68dd5d2346d575658db5140945d154727a
tsmtang <t349359058@sina.com>
2021-03-24 15:32:14 +0800

    hot question

--- app/Http/Controllers/V1/QuestionController.php
+++ app/Http/Controllers/V1/QuestionController.php
@@ -17,9 +17,13 @@ class QuestionController extends BaseController
 
     public function hot(Request $request)
     {
-        $questions = Question::where('status' ,1)->select('id' , 'title')->paginate(10);
-        $questions->each(function($question , $index){
-            $question->url = 'https://baidu.com';
+        $locale = locale();
+        $questions = Question::where('status' ,1)->select('id' , 'title', 'url')->paginate(10);
+        $questions->each(function($question)use($locale){
+            $title = json_decode($question->title, true);
+            $url   = json_decode($question->url, true);
+            $question->title = !empty($title[$locale]) ? $title[$locale] : $title['en'];
+            $question->url   = !empty($url[$locale])   ? $url[$locale]   : $url['en'];
         });
         return AnonymousCollection::collection($questions);
     }
