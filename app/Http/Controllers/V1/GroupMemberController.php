f262930e8592884396e52ccb8fa893ec521f4d77
2236559110@qq.com <2236559110@qq.com>
2021-04-12 17:41:48 +0800

    fix group

--- app/Http/Controllers/V1/GroupMemberController.php
+++ app/Http/Controllers/V1/GroupMemberController.php
@@ -163,6 +163,7 @@ class GroupMemberController extends BaseController
         DB::beginTransaction();
         try{
             $deleteMemberResult = DB::table('groups_members')->where('id' , $groupMember['id'])->delete();
+            unset($groupMember['id']);
             $insertMemberLogResult = DB::table('groups_members_logs')->insert($groupMember);
             $updateGroupResult = DB::table('groups')->where('id' , $id)->update($groupData);
             !$deleteMemberResult&&abort(405 , 'Group member delete failed!');
