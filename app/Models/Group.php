7944fe791a21982c2317f4b256cb76b5575e10c4
2236559110@qq.com <2236559110@qq.com>
2021-04-19 15:02:06 +0800

    fix group name and avatar

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -11,8 +11,6 @@ class Group extends Model
     protected $table = "groups";
     protected $casts = [
         'id' => 'string',
-        'name' => 'array',
-        'avatar' => 'array'
     ];
     protected $fillable = ['user_id' , 'administrator' , 'name' , 'avatar' , 'member' , 'is_deleted' , 'deleted_at'];
 
@@ -28,5 +26,27 @@ class Group extends Model
         'updated_at',
         'deleted_at',
     ];
+
+    public function getNameAttribute($name)
+    {
+        $name = \json_decode($name , true);
+        if(is_array($name))
+        {
+            $name = array_values($name);
+            $name = implode(',' , $name);
+        }
+        return $name==null?'':$name;
+    }
+
+    public function getAvatarAttribute($avatar)
+    {
+        $avatar = \json_decode($avatar , true);
+        if(is_array($avatar))
+        {
+            $avatar = array_values($avatar);
+            $avatar = implode(',' , $avatar);
+        }
+        return $avatar==null?'':$avatar;
+    }
 
 }
