b3d7203d3c009ff334132b17ae558f39f33364b5
2236559110@qq.com <2236559110@qq.com>
2021-04-20 15:16:17 +0800

    fix group

--- app/Jobs/GroupMemberJoin.php
+++ app/Jobs/GroupMemberJoin.php
@@ -4,6 +4,7 @@ namespace App\Jobs;
 
 use Illuminate\Bus\Queueable;
 use App\Resources\UserCollection;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
@@ -19,12 +20,14 @@ class GroupMemberJoin implements ShouldQueue
     private $group;
     private $userIds;
     private $user;
+    private $now;
 
     public function __construct($group , $user , $userIds)
     {
         $this->group = $group;
         $this->user = $user;
         $this->userIds = $userIds;
+        $this->now = date("Y-m-d H:i:s");
     }
 
     /**
@@ -73,6 +76,46 @@ class GroupMemberJoin implements ShouldQueue
         Log::info('group_notice_request' , $content);
         $result = app('rcloud')->getMessage()->Group()->send($content);
         Log::info('group_notice_result' , $result);
+
+        if($this->group->name_isset==0)
+        {
+            $names = $this->group->getOriginal('name');
+            $names = \json_decode($names , true);
+            if(is_array($names))
+            {
+                if(count($names)<4)
+                {
+                    $joinNames = array_slice(collect($members)->pluck('user_nick_name' , 'user_id')->toArray() , 0 , 4-count($names) , true);
+                    $names = $names+$joinNames;
+                    DB::table('groups')->where('id' , $groupId)->update(array(
+                        'name'=>\json_encode($names),
+                        'updated_at'=>$this->now,
+                    ));
+                    $group = Group::where('id' , $groupId)->first();
+                    GroupUpdate::dispatch($group , $this->user)->onQueue('helloo_{group_operate}');
+                }
+            }
+
+        }
+        if($this->group->avatar_isset==0)
+        {
+            $avatars = $this->group->getOriginal('avatar');
+            $avatars = \json_decode($avatars , true);
+            if(is_array($avatars))
+            {
+                if(count($avatars)<4)
+                {
+                    $joinAvatars = array_slice(collect($members)->pluck('user_avatar_link' , 'user_id')->toArray() , 0 , 4-count($avatars) , true);
+                    $avatars = $avatars+$joinAvatars;
+                    DB::table('groups')->where('id' , $groupId)->update(array(
+                        'avatar'=>\json_encode($avatars),
+                        'updated_at'=>$this->now,
+                    ));
+                    $group = Group::where('id' , $groupId)->first();
+                    GroupUpdate::dispatch($group , $this->user)->onQueue('helloo_{group_operate}');
+                }
+            }
+        }
     }
 
 }
