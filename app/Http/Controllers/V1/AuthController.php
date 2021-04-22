f1a73b8e52adb35dd1d8d502227d4e54751b7320
tsmtang <t349359058@sina.com>
2021-04-22 10:35:26 +0800

    block user device

--- app/Http/Controllers/V1/AuthController.php
+++ app/Http/Controllers/V1/AuthController.php
@@ -45,18 +45,9 @@ class AuthController extends BaseController
         {
             abort(401 , __('Please update to the latest version from Play Store.'));
         }
-        $deviceKey      = 'block_user';
-        $time = Redis::sismember($deviceKey , $deviceId);
-        if(!empty($time))
-        {
-            abort(401 , trans('auth.user_banned'));
-        }
-        $deviceKey      = 'block_device';
-        $time = Redis::sismember($deviceKey , $deviceId);
-        if(!empty($time))
-        {
-            abort(401 , trans('auth.user_device_banned'));
-        }
+
+        $time = Redis::sismember('block_device' , $deviceId);
+        $time && abort(401 , trans('auth.user_device_banned'));
 
         $user_phone = ltrim(ltrim(strval($request->input('user_phone' , "")) , "+") , "0");
         $user_phone_country = ltrim(strval($request->input('user_phone_country' , "86")) , "+");
@@ -96,6 +87,9 @@ class AuthController extends BaseController
         {
             return $this->response->errorUnauthorized(__("Phone number hasn't been registered yet."));
         }
+        $exist = Redis::sismember('block_user' , $phone->user_id);
+        $exist && abort(401 , trans('auth.user_banned'));
+
         $user = $this->user->find($phone->user_id);
         if(!config('common.is_verification')||password_verify($password, $user->user_pwd))
         {
