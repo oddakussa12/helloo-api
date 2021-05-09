7aab8625674e5d3215dd3e1473aab4475ea403bd
2236559110@qq.com <2236559110@qq.com>
2021-05-09 15:36:03 +0800

    shop and goods

---
+++ app/Http/Controllers/V1/ShopController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+
+use Illuminate\Http\Request;
+
+class ShopController extends BaseController
+{
+    public function index()
+    {
+
+    }
+
+    public function show($id)
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function update(Request $request , $id)
+    {
+
+    }
+}
