873194559a639d4725df1dc57771264ffa618ebb
2236559110@qq.com <2236559110@qq.com>
2021-05-14 13:26:43 +0800

    fix ding

--- app/Custom/GeoIP/GeoIP.php
+++ app/Custom/GeoIP/GeoIP.php
@@ -165,7 +165,6 @@ class GeoIP
                     $log = new Logger('geoip');
                     $log->pushHandler(new StreamHandler(storage_path('logs/geoip.log'), Logger::ERROR));
                     $log->error($e);
-//                    app(DingTalk::class)->with('default')->text((string)$e);
                 }
             }
         }
