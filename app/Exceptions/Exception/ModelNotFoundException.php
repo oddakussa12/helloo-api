bab334b6170a5911873d4aba123f163f8321e578
2236559110@qq.com <2236559110@qq.com>
2021-05-14 14:18:21 +0800

    fix exception

--- app/Exceptions/Exception/ModelNotFoundException.php
+++ app/Exceptions/Exception/ModelNotFoundException.php
@@ -4,9 +4,7 @@ namespace App\Exceptions\Exception;
 
 use RuntimeException;
 use App\Models\User;
-use App\Models\Post;
 use Illuminate\Support\Arr;
-use App\Models\PostComment;
 
 class ModelNotFoundException extends RuntimeException
 {
@@ -25,23 +23,11 @@ class ModelNotFoundException extends RuntimeException
         if($model==User::class)
         {
             $message = 'Sorry, this account does not exist or is blocked!';
-        }elseif ($model==Post::class)
-        {
-            $message = 'Sorry, this post does not exist or was deleted!';
-        }elseif ($model==PostComment::class)
-        {
-            $message = 'Sorry, this comment does not exist or has been deleted!';
         }else{
 
             $message = 'Sorry, this resource does not exist or has been deleted!';
         }
         $this->message = $message;
-
-//        if (count($this->ids) > 0) {
-//            $this->message .= ' '.implode(', ', $this->ids);
-//        } else {
-//            $this->message .= '.';
-//        }
 
         return $this;
     }
