d6a9fd739d900713ab2a881fe9d7408fdc746bcd
2236559110@qq.com <2236559110@qq.com>
2021-06-10 17:08:34 +0800

    fix generate discovery

--- app/Console/Commands/GenerateDiscovery.php
+++ app/Console/Commands/GenerateDiscovery.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redis;
@@ -39,12 +40,95 @@ class GenerateDiscovery extends Command
      */
     public function handle()
     {
-        $limit = 10;
-        $offset = 0;
+        $this->popularShops();
+        $this->ratedShops();
+        $this->popularProducts();
+        $this->ratedProducts();
+    }
+
+    private function popularShops()
+    {
+        $lastWeek = Carbon::now()->subWeek(1)->startOfWeek()->toDateTimeString();
+        $limit = 50;
         $flag = true;
+        $page = 0;
+        $key = "helloo:discovery:popular:shops";
+        do{
+            $offset = $page*$limit;
+            $sql = 'select `owner`,count(`id`) as `num` from `t_shops_views_logs` where `created_at` >= '.$lastWeek.' group by `owner` order by `num` desc '.$limit.' offset '.$offset.';';
+            $views = DB::select($sql);
+            if(blank($views))
+            {
+                $flag = false;
+            }else{
+                $data = array();
+                foreach ($views as $view)
+                {
+                    $data[$view->owner] = $view->num;
+                }
+                dump($data);
+                Redis::zadd($key , $data);
+            }
+            $page ++;
+        }while($flag);
+    }
+
+    private function ratedShops()
+    {
+        $key = "helloo:discovery:rated:shop";
+        DB::table('shop_evaluation_points')->orderByDesc('user_id')->chunk(100 , function($shops) use ($key){
+            $data = array();
+            foreach ($shops as $shop)
+            {
+                $point = $shop->point_1+$shop->point_2*2+$shop->point_3*3+$shop->point_4*4+$shop->point_5*5;
+                $num = $shop->point_1+$shop->point_2+$shop->point_3+$shop->point_4+$shop->point_5;
+                if($num>0)
+                {
+                    $data[$shop->user_id] = round($point/$num , 1);
+                }
+            }
+            !empty($data)&&Redis::zadd($key , $data);
+        });
+    }
+
+
+    private function popularProducts()
+    {
+        $lastWeek = Carbon::now()->subWeek(1)->startOfWeek()->toDateTimeString();
+        $limit = 50;
+        $flag = true;
+        $page = 0;
         $key = "helloo:discovery:popular:products";
         do{
-            $points = DB::select('select round(`point`/`comment`) as `a_point`,`id` , `created_at` from `t_goods` where `comment`>0 order by `a_point` desc,`created_at` desc limit '.$limit.' offset '.$offset.';');
+            $offset = $page*$limit;
+            $sql = 'select `goods_id`,count(`id`) as `num` from `t_goods_views_logs` where `created_at` >= '.$lastWeek.' group by `goods_id` order by `num` desc '.$limit.' offset '.$offset.';';
+            $views = DB::select($sql);
+            if(blank($views))
+            {
+                $flag = false;
+            }else{
+                $data = array();
+                foreach ($views as $view)
+                {
+                    $data[$view->goods_id] = $view->num;
+                }
+                dump($data);
+                Redis::zadd($key , $data);
+            }
+            $page ++;
+        }while($flag);
+    }
+
+    private function ratedProducts()
+    {
+        $limit = 50;
+        $flag = true;
+        $page = 0;
+        $key = "helloo:discovery:rated:products";
+        do{
+            $offset = $page*$limit;
+            $sql = 'select round(`point`/`comment` , 1) as `a_point`,`id` , `created_at` from `t_goods` where `comment`>0 order by `a_point` desc,`created_at` desc limit '.$limit.' offset '.$offset.';';
+            $points = DB::select($sql);
             if(blank($points))
             {
                 $flag = false;
@@ -54,8 +138,10 @@ class GenerateDiscovery extends Command
                 {
                     $data[$point->id] = $point->a_point;
                 }
+                dump($data);
                 Redis::zadd($key , $data);
             }
+            $page ++;
         }while($flag);
     }
 
