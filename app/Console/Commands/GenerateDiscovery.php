1cd97bb5be7911800d4b857b3e6b3c3e30f029b4
2236559110@qq.com <2236559110@qq.com>
2021-06-10 15:29:52 +0800

    fix discovery

---
+++ app/Console/Commands/GenerateDiscovery.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class GenerateDiscovery extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'generate:discovery';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate Discovery';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $limit = 10;
+        $offset = 0;
+        $flag = true;
+        $goodsPoint = DB::select('select round(`point`/`comment`) as `a_point`,`id` , `created_at` from `t_goods` where `comment`>0 order by `a_point` desc,`created_at` desc limit '.$limit.' offset '.$offset.';');
+        do{
+            dd($goodsPoint);
+        }while($flag);
+    }
+
+}
