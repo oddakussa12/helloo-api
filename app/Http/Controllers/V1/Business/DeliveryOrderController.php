8e7b98ec0f40bf397fc4ec03ddfb665c141f71f3
2236559110@qq.com <2236559110@qq.com>
2021-06-11 20:51:50 +0800

    fix shipday

--- app/Http/Controllers/V1/Business/DeliveryOrderController.php
+++ app/Http/Controllers/V1/Business/DeliveryOrderController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers\V1\Business;
 
+use App\Jobs\Shipday;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\V1\BaseController;
+use App\Repositories\Contracts\UserRepository;
+use App\Repositories\Contracts\GoodsRepository;
 use App\Http\Requests\StoreDeliveryOrderRequest;
 
 class DeliveryOrderController extends BaseController
@@ -20,8 +23,9 @@ class DeliveryOrderController extends BaseController
         $userContact = $request->input('user_contact' , '');
         $userAddress = $request->input('user_address' , '');
         $createdAt = $updatedAt = date('Y-m-d H:i:s');
+        $orderId = app('snowflake')->id();
         $orderResult = DB::table('delivery_orders')->insert(array(
-            'order_id'=>app('snowflake')->id(),
+            'order_id'=>$orderId,
             'user_id'=>$userId,
             'owner'=>$owner,
             'goods_id'=>$goodsId,
@@ -37,6 +41,18 @@ class DeliveryOrderController extends BaseController
                 'user_id'=>$userId,
                 'data'=>$request->all()
             ));
+        }else{
+            $orderItem = [];
+            $user = app(UserRepository::class)->findByUserId($owner);
+            if(!empty($goodsId))
+            {
+                $goods = app(GoodsRepository::class)->find($goodsId);
+                $orderItem = [$goods->name];
+            }
+            if(!blank($user))
+            {
+                Shipday::dispatch($orderId , $userName , $userAddress , $userContact , strval($user->get('user_nick_name')) , strval($user->get('user_address')) , strval($user->get('user_contact')) , $orderItem , 0 , 0)->onQueue('helloo_{delivery_shipday}');
+            }
         }
         return $this->response->created();
     }
