9b0708869ab680d35f73fe7e3228acc6f4638b87
2236559110@qq.com <2236559110@qq.com>
2021-06-26 11:59:13 +0800

    fix shop tag

--- app/Http/Controllers/V1/Business/ShopTagController.php
+++ app/Http/Controllers/V1/Business/ShopTagController.php
@@ -8,19 +8,27 @@ use App\Models\Business\ShopTag;
 use App\Resources\AnonymousCollection;
 use App\Http\Controllers\V1\BaseController;
 use App\Models\Business\ShopTagTranslation;
+use Illuminate\Support\Facades\Redis;
 
 class ShopTagController extends BaseController
 {
     public function index(Request $request)
     {
-        $locale = locale();
         $goodsTags = ShopTag::all();
         $tagIds = $goodsTags->pluck('id')->toArray();
-        $goodsTagsTranslations = ShopTagTranslation::whereIn('tag_id' , $tagIds)->whereIn('locale' , array($locale , 'en'))->get();
+        $goodsTagsTranslations = ShopTagTranslation::whereIn('tag_id' , $tagIds)->get();
         $goodsTags->each(function($goodsTag) use ($goodsTagsTranslations){
             $goodsTag->translations = $goodsTagsTranslations->where('tag_id' , $goodsTag->id);
         });
-        return AnonymousCollection::collection($goodsTags);
+        $key = 'helloo:business:service:tags';
+        if(Redis::exists($key))
+        {
+            $data = \json_decode(Redis::get($key , true));
+        }else{
+            $data = $goodsTags->toArray();
+            Redis::set($key , \json_encode($data , JSON_UNESCAPED_UNICODE));
+        }
+        return AnonymousCollection::collection(collect($data));
     }
 
 }
