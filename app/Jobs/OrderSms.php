df69b085518607fe7427fdb4ff6bf6c67724f9ea
tsmtang <t349359058@sina.com>
2021-06-28 13:28:52 +0800

    order sms  add header

--- app/Jobs/OrderSms.php
+++ app/Jobs/OrderSms.php
@@ -17,11 +17,13 @@ class OrderSms implements ShouldQueue
 
     private $orderInfo;
     private $type;
+    private $order_sms_domain;
 
     public function __construct($orderInfo , $type = 'single')
     {
         $this->orderInfo = $orderInfo;
         $this->type = $type;
+        $this->order_sms_domain = config('common.order_sms_domain');
     }
 
     /**
@@ -75,7 +77,8 @@ class OrderSms implements ShouldQueue
             CURLOPT_CUSTOMREQUEST => 'POST',
             CURLOPT_POSTFIELDS =>\json_encode($params),
             CURLOPT_HTTPHEADER => array(
-                'Content-Type: application/json'
+                'Content-Type: application/json',
+                'hello: '.$this->order_sms_domain
             ),
         ));
 
