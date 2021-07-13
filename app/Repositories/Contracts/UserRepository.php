628d32d50514d906ce57ed4dbc93cbd2a6785824
2236559110@qq.com <2236559110@qq.com>
2021-07-13 12:50:26 +0800

    fix UserRepository

--- app/Repositories/Contracts/UserRepository.php
+++ app/Repositories/Contracts/UserRepository.php
@@ -5,9 +5,6 @@ namespace App\Repositories\Contracts;
 
 interface UserRepository extends BaseRepository
 {
-    public function isDeletedUser($name);
-
-    public function isBlackUser($user_id);
 
     public function findByUserIds($userIds);
 
