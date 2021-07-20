646b9b5782f246dc619f060000c1285a4683cccc
2236559110@qq.com <2236559110@qq.com>
2021-07-20 15:47:38 +0800

    fix format_promo_price

--- app/Models/Business/Order.php
+++ app/Models/Business/Order.php
@@ -18,7 +18,7 @@ class Order extends Model
 
     protected $hidden = ['schedule' , 'shop_price' , 'comment' , 'order_time' , 'operator' , 'deposit' , 'updated_at' , 'brokerage_percentage' , 'brokerage' , 'profit'];
 
-    protected $appends = ['format_price'];
+    protected $appends = ['format_price' , 'format_promo_price'];
 
     protected $casts = [
         'user_id' => 'string',
